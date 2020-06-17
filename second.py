72f085f9e8961a5bcf031e9614f6a1d83df27f81
yoshi-ki <yoshikif98@gmail.com>
2020-06-17 22:36:23 +0900

    一直線のグラフなら対応可能

---
+++ second.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+def as_array(x):
+  if np.isscalar(x):
+    return np.array(x)
+  return x
+
+
+class Variable:
+  def __init__(self, data):
+    # ndarrayとNone以外は受け付けないようにする
+    if data is not None:
+      if not isinstance(data, np.ndarray):
+        raise TypeError('{} is not supported'.format(type(data)))
+    self.data = data
+    self.grad = None  # その変数をxとして、全体の出力をyとして、dy/dxが変数ごとに入る
+    self.creator = None
+
+  def set_creator(self, func):
+    self.creator = func
+
+  def backward(self):
+    if self.grad is None:
+      self.grad = np.ones_like(self.data)
+
+    funcs = [self.creator]
+    while funcs:
+      f = funcs.pop()  # 今わかっていないnodeのgradを入力として、既知のgradのnodeを出力する関数を取得
+      gys = [output.grad for output in f.outputs]  # 関数の出力を取得
+      # このbackward methodでは、dw/dxを計算して、dy/dwと掛け合わせることでdy/dxを計算してる(関数への入力がややこしいけど)
+      gxs = f.backward(*gys)
+      if not isinstance(gxs, tuple):
+        gxs = (gxs,)
+      for x, gx in zip(f.inputs, gxs):  # 関数の入力を取得
+        if x.grad is None:
+          x.grad = gx
+        else:
+          x.grad = x.grad + gx
+        if x.creator is not None:
+          funcs.append(x.creator)  # 一つ前の関数をリストに追加する
+
+  def cleargrad(self):
+    self.grad = None
+
+
+class Function:
+  def __call__(self, *inputs):
+    xs = [x.data for x in inputs]
+    ys = self.forward(*xs)
+    if not isinstance(ys, tuple):
+      ys = (ys,)
+    outputs = [Variable(as_array(y)) for y in ys]
+    for output in outputs:
+      output.set_creator(self)
+    self.inputs = inputs
+    self.outputs = outputs
+    # 要素が一つのときは要素を返し、それ以外の時はリストを返す
+    return outputs if len(outputs) > 1 else outputs[0]
+
+  def forward(self, xs):
+    raise NotImplementedError()
+
+  def backward(self, gys):
+    raise NotImplementedError()
+
+
+class Square(Function):
+  def forward(self, x):
+    return x ** 2
+
+  def backward(self, gy):
+    x = self.inputs[0].data
+    gx = 2 * x * gy
+    return gx
+
+
+class Exp(Function):
+  def forward(self, x):
+    return np.exp(x)
+
+  def backward(self, gy):
+    x = self.input.data
+    gx = np.exp(x) * gy
+    return gx
+
+
+class Add(Function):
+  def forward(self, x0, x1):
+    y = x0 + x1
+    return y
+
+  def backward(self, gy):
+    return gy, gy
+
+
+def numerical_diff(f, x, eps=1e-4):
+  # 中心差分での実装
+  x0 = Variable(x.data - eps)
+  x1 = Variable(x.data + eps)
+  y0 = f(x0)
+  y1 = f(x1)
+  return (y1.data - y0.data) / (2 * eps)
+
+
+def square(x):
+  return Square()(x)
+
+
+def exp(x):
+  return Exp()(x)
+
+
+def add(x0, x1):
+  return Add()(x0, x1)
+
+
+x = Variable(np.array(3.0))
+y = add(x, x)
+y.backward()
+print(x.grad)  # 2.0
+# 2回目の計算(同じxを使って、別の計算を行う)
+x.cleargrad()
+y = add(add(x, x), x)
+y.backward()
+print(x.grad)
