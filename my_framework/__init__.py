82d33d74803b3dbb94871d90ba64926ab380a601
yoshi-ki <yoshikif98@gmail.com>
2020-06-19 00:57:21 +0900

    高階微分の実装:

--- my_framework/__init__.py
+++ my_framework/__init__.py
@@ -1,9 +1,19 @@
-from my_framework.core_simple import Variable
-from my_framework.core_simple import Function
-from my_framework.core_simple import using_config
-from my_framework.core_simple import no_grad
-from my_framework.core_simple import as_array
-from my_framework.core_simple import as_variable
-from my_framework.core_simple import setup_variable
+is_simple_core = False
 
+if is_simple_core:
+  from my_framework.core_simple import Variable
+  from my_framework.core_simple import Function
+  from my_framework.core_simple import using_config
+  from my_framework.core_simple import no_grad
+  from my_framework.core_simple import as_array
+  from my_framework.core_simple import as_variable
+  from my_framework.core_simple import setup_variable
+else:
+  from my_framework.core import Variable
+  from my_framework.core import Function
+  from my_framework.core import using_config
+  from my_framework.core import no_grad
+  from my_framework.core import as_array
+  from my_framework.core import as_variable
+  from my_framework.core import setup_variable
 setup_variable()
