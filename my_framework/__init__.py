16b86a1a77fc663b86182917b283810cb21a5b34
yoshi-ki <yoshikif98@gmail.com>
2020-06-18 11:37:23 +0900

    projectの整理

---
+++ my_framework/__init__.py
@@ -0,0 +1,9 @@
+from my_framework.core_simple import Variable
+from my_framework.core_simple import Function
+from my_framework.core_simple import using_config
+from my_framework.core_simple import no_grad
+from my_framework.core_simple import as_array
+from my_framework.core_simple import as_variable
+from my_framework.core_simple import setup_variable
+
+setup_variable()
