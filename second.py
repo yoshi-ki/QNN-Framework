f7cf0d25b9f052a469bbb58d7c4e5af3e307ecd9
yoshi-ki <yoshikif98@gmail.com>
2020-06-18 11:01:52 +0900

    割り算の実装

--- second.py
+++ second.py
@@ -235,6 +235,32 @@ Variable.__sub__ = sub
 Variable.__rsub__ = rsub
 
 
+class Div(Function):
+  def forward(self, x0, x1):
+    y = x0 / x1
+    return y
+
+  def backward(self, gy):
+    x0, x1 = self.inputs[0].data, self.inputs[1].data
+    gx0 = gy / x1
+    gx1 = gy * (-x0 / x1 ** 2)
+    return gx0, gx1
+
+
+def div(x0, x1):
+  x1 = as_array(x1)
+  return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+  x1 = as_array(x1)
+  return Div()(x1, x0)
+
+
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+
+
 def numerical_diff(f, x, eps=1e-4):
   # 中心差分での実装
   x0 = Variable(x.data - eps)
