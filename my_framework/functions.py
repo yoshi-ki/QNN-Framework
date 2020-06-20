0582e0742cb832881e00e713b934554b3a6a7d2f
yoshi-ki <yoshikif98@gmail.com>
2020-06-20 23:36:25 +0900

    ニューラルネットワークが動く

--- my_framework/functions.py
+++ my_framework/functions.py
@@ -49,6 +49,21 @@ def tanh(x):
   return Tanh()(x)
 
 
+class Exp(Function):
+  def forward(self, x):
+    y = np.exp(x)
+    return y
+
+  def backward(self, gy):
+    y = self.outputs[0]()  # weakref
+    gx = gy * y
+    return gx
+
+
+def exp(x):
+  return Exp()(x)
+
+
 class Reshape(Function):
   def __init__(self, shape):
     self.shape = shape
@@ -174,3 +189,47 @@ class MeanSquaredError(Function):
 
 def mean_squared_error(y, y_pred):
   return MeanSquaredError()(y, y_pred)
+
+
+class Linear(Function):
+  def forward(self, x, W, b):
+    y = x.dot(W)
+    if b is not None:
+      y += b
+    return y
+
+  def backward(self, gy):
+    x, W, b = self.inputs
+    gb = None if b.data is None else sum_to(gy, b.shape)
+    gx = matmul(gy, W.T)
+    gW = matmul(x.T, gy)
+    return gx, gW, gb
+
+
+def linear(x, W, b=None):
+  return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+  x, W = as_variable(x), as_variable(W)
+  t = matmul(x, W)
+  if b is None:
+    return t
+  y = t + b
+  t.data = None
+  return y
+
+
+class Sigmoid(Function):
+  def forward(self, x):
+    y = 1 / (1 + np.exp(-x))
+    return y
+
+  def backward(self, gy):
+    y = self.outputs[0]()
+    gx = gy * y * (1 - y)
+    return gx
+
+
+def sigmoid(x):
+  return Sigmoid()(x)
