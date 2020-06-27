e51bca1df79d9e684a8b061cac18db52426ce4f7
yoshi-ki <yoshikif98@gmail.com>
2020-06-27 21:02:38 +0900

    Q_MLPのclass作成

--- my_framework/models.py
+++ my_framework/models.py
@@ -27,3 +27,27 @@ class MLP(Model):
       x = self.activation(l(x))
     return self.layers[-1](x)
 
+# quantizeされた全結合ニューラルネットワーク
+
+
+class Q_MLP(Model):
+  # MLPは全結合NNの抽象クラス
+  def __init__(self, fc_output_sizes, activation=F.sigmoid):
+    super().__init__()
+    self.activation = activation
+    self.layers = []
+
+    for i, out_size in enumerate(fc_output_sizes):
+      # 最終層はquantizeしない
+      if i == (len(fc_output_sizes) - 1):
+        layer = L.Linear(out_size)
+      else:
+        # 入力を受け取って、計算を行ってquantizeを行う
+        layer = L.Qlinear(out_size)
+      setattr(self, 'l' + str(i), layer)
+      self.layers.append(layer)
+
+  def forward(self, x):
+    for l in self.layers[:-1]:
+      x = self.activation(l(x))
+    return self.layers[-1](x)
